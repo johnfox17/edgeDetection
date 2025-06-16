ddc855d0a768bc2c79e0fa9430ebc44bb8b264b0
johnfox17 <johnfox17@gmail.com>
2025-06-16 16:46:44 +0100

    Included changes recommended by Professor Rodriguez

--- src/edgeDetectorMethods/Argyle/argyleEdgeDetector.m
+++ src/edgeDetectorMethods/Argyle/argyleEdgeDetector.m
@@ -4,10 +4,19 @@ function [gradientMagnitude] = argyleEdgeDetector(image, sigma, Nx, Ny)
     yradius = ceil((Ny-1)/2);
     [x,y] = meshgrid(-xradius:xradius,-yradius:yradius);
     h1 = sign(x).*exp(-0.5*x.^2/sigma^2);
-    h1 = h1 ./(2*sum(h1(:,xradius+1:end),'all'));
+    %h1 = h1 ./(2*sum(h1(:,xradius+1:end),'all'));
+    s = sum(h1(:,xradius+2:end),'all');
+    xcentroid = sum(h1(:,xradius+2:end).*x(:,xradius+2:end),'all')/s;
+    d = 2*xcentroid;
+    h1 = h1 / (s*d);
 
     h2 = sign(y).*exp(-0.5*y.^2/sigma^2);
-    h2 = h2 ./(2*sum(h2(1:yradius,:),'all'));
+    %h2 = h2 ./(2*sum(h2(1:yradius,:),'all'));
+    s = sum(h2(yradius+2:end,:),'all');
+    ycentroid = sum(h1(yradius+2:end).*y(yradius+2:end),'all')/s;
+    d = 2*ycentroid;
+    h2 = h2 / (s*d);
+
     %Calculate Gradient Magnitude
     xGradient = conv2(image, h1,'same');
     yGradient = conv2(image, h2,'same');    
--- src/edgeDetectorMethods/Macleod/macleodEdgeDetector.m
+++ src/edgeDetectorMethods/Macleod/macleodEdgeDetector.m
@@ -6,14 +6,23 @@ function [gradientMagnitude] = macleodEdgeDetector(image, sigma1,sigma2, Nx, Ny)
     g1xminus = exp(-0.5*(x-sigma1).^2/sigma1^2);
     g1xplus  = exp(-0.5*(x+sigma1).^2/sigma1^2);
     h1 = g2y .* (g1xminus - g1xplus);
-    h1 = h1 ./ (2*sum(h1(:,xradius+1:end),'all'));
+    %h1 = h1 ./ (2*sum(h1(:,xradius+1:end),'all'));
+    s = sum(h1(:,xradius+2:end),'all');
+    xcentroid = sum(h1(:,xradius+2:end).*x(:,xradius+2:end),'all')/s;
+    d = 2*xcentroid;
+    h1 = h1 / (s*d);
     
     [x,y] = meshgrid(-xradius:xradius,-yradius:yradius);
     g2x = exp(-0.5*x.^2/sigma2^2);
     g1yminus = exp(-0.5*(y-sigma1).^2/sigma1^2);
     g1yplus  = exp(-0.5*(y+sigma1).^2/sigma1^2);
     h2 = g2x .* (g1yminus - g1yplus);
-    h2 = h2 ./(2*sum(h2(1:yradius,:),'all'));
+    %h2 = h2 ./(2*sum(h2(1:yradius,:),'all'));
+    s = sum(h2(yradius+2:end,:),'all');
+    ycentroid = sum(h1(yradius+2:end).*y(yradius+2:end),'all')/s;
+    d = 2*ycentroid;
+    h2 = h2 / (s*d);
+
     %Calculate Gradient Magnitude
     xGradient = conv2(image, h1,'same');
     yGradient = conv2(image, h2,'same');    
